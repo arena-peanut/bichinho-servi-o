42be07687c6e45fe5df9e13d2fe4ec413865e051
jessyanjos <jessicarocha.anjos@gmail.com>
2017-12-29 12:39:28 -0300

    Adicionando teste simples

---
+++ hello_test.py
@@ -0,0 +1,16 @@
+import json
+import pytest
+from hello import app
+
+@pytest.fixture
+def client(request):
+    test_client = app.test_client()
+    return test_client
+
+def json_of_response(response):
+    return json.loads(response.data.decode('utf8'))
+
+def testGetHelloWorldWhenRequestingHello(client):
+    response = client.get('/hello')
+    assert response.status_code == 200
+    assert json_of_response(response) == {"hello": "hello world"}
