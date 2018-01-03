e5b169cdb9195be456cb0e7df6f1697cef860394
ladeia <you@example.com>
2018-01-02 22:20:08 -0300

    add new endpoint /

--- app/hello.py
+++ app/hello.py
@@ -2,9 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'nome': 'bichinho service'})
+
+
 @app.route('/hello', methods=['GET'])
 def hello():
     return jsonify({'hello': 'hello world'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test/hello_test.py
+++ test/hello_test.py
@@ -2,15 +2,24 @@ import json
 import pytest
 from app.hello import app
 
+
 @pytest.fixture
-def client(request):
+def client():
     test_client = app.test_client()
     return test_client
+
 
 def json_of_response(response):
     return json.loads(response.data.decode('utf8'))
 
-def testGetHelloWorldWhenRequestingHello(client):
+
+def test_get_hello_world_when_requesting_hello(client):
     response = client.get('/hello')
     assert response.status_code == 200
     assert json_of_response(response) == {"hello": "hello world"}
+
+
+def test_get_name_when_requesting_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert json_of_response(response) == {'nome': 'bichinho service'}
